42b797526b80531d20cdd343fdcf2c9933b25013
Andrei Augustin <andreiaugustin98@gmail.com>
2022-05-10 20:47:55 +0100

    reordered

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,31 +1,31 @@
 const portfolio = {
-    ChrisAndMelissa : {
-        img: "/img/portfolio/ChrisAndMelissa.jpg",
-        url: "/portfolio/ChrisAndMelissa"
-    },
-    NeeshaAndNitin : {
-        img: "/img/portfolio/NeeshaAndNitin.jpg",
-        url: "/portfolio/NeeshaAndNitin"
+    CrystalAndLewis : {
+        img: "/img/portfolio/CrystalAndLewis.jpg",
+        url: "/portfolio/CrystalAndLewis"
     },
     SamanthaAndJaideep : {
         img: "/img/portfolio/SamanthaAndJaideep.jpg",
         url: "/portfolio/SamanthaAndJaideep"
     },
+    PeterAndSandy : {
+        img: "/img/portfolio/PeterAndSandy.jpg",
+        url: "/portfolio/PeterAndSandy"
+    },
     LaurenAndBen : {
         img: "/img/portfolio/LaurenAndBen.jpg",
         url: "/portfolio/LaurenAndBen"
     },
+    NeeshaAndNitin : {
+        img: "/img/portfolio/NeeshaAndNitin.jpg",
+        url: "/portfolio/NeeshaAndNitin"
+    },
+    ChrisAndMelissa : {
+        img: "/img/portfolio/ChrisAndMelissa.jpg",
+        url: "/portfolio/ChrisAndMelissa"
+    },
     RoshnaAndAlistair : {
         img: "/img/portfolio/RoshnaAndAlistair.jpg",
         url: "/portfolio/RoshnaAndAlistair"
-    },
-    CrystalAndLewis : {
-        img: "/img/portfolio/CrystalAndLewis.jpg",
-        url: "/portfolio/CrystalAndLewis"
-    },
-    PeterAndSandy : {
-        img: "/img/portfolio/PeterAndSandy.jpg",
-        url: "/portfolio/PeterAndSandy"
     },
     SarahAndJames : {
         img: "/img/portfolio/SarahAndJames.jpg",
