55ecce90a182a2ebfce99c6bba48ca089f9ef1cb
Andrei Augustin <andreiaugustin98@gmail.com>
2022-05-13 10:58:06 +0100

    margin correction

--- src/components/ThinNavigationBarWhite.vue
+++ src/components/ThinNavigationBarWhite.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="bg-transparent flex flex-row justify-between min-w-[80%] 2xl:min-w-[20%] xl:min-w-[25%] lg:min-w-[30%] md:min-w-[40%] sm:min-w-[50%]">
-  <router-link to="/portfolio" class="border-2 p-2 border-white">
+<div class="bg-transparent flex flex-row justify-between">
+  <router-link to="/portfolio" class="border-2 p-2 border-white mr-4">
     <h2 class="nav-text text-black dark:text-white ">Portfolio</h2>
   </router-link>
-  <a href="mailto:selinafinke@gmail.com" class="border-2 p-2 border-white"><h2 class="nav-text text-black dark:text-white ">Contact</h2></a>
+  <a href="mailto:selinafinke@gmail.com" class="border-2 p-2 border-white ml-4"><h2 class="nav-text text-black dark:text-white ">Contact</h2></a>
 </div>
 </template>
 
